a232287e9dba6bc6ae728757683eb0dc34f1f2d5
Tomiblanchard <tomiblanchard@gmail.com>
2015-03-25 11:21:02 +0000

    steve demo

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,37 @@
 # board
 class Board
+  DEFAULT_SIZE = 1
+  attr_reader :grid
 
-  def dimensions
-    
+  def initialize options
+    size = options.fetch(:size, DEFAULT_SIZE)
+    cell = options.fetch(:cell)
+    @grid = {}
+    letter_range_based_on_size(size).map do |letter|
+      (1..dimension_size(size)).map{|number| @grid['#{letter}#{number}'.to_sym] = cell.new}
+  end
+
+  def dimension_size size
+    Math.sqrt(size).ceil
+  end
+
+  def letter_range_based_on_size size
+    ('A'..to_letter_in_alphabet(dimension_size(size))
+  end
+
+  def to_letter_in_alphabet number
+    (number.ord + 64).chr
+  end
+
+  def place ship, coordinate
+    coordinates_for(ship.size, coordinate).each do |coordinate|
+    grid[coordinate].content = ship
+  end
+
+  def coordinates_for size, coordinates
+    coordinates = [coordinate]
+    (size - 1).times {coordinates << coordinates.last.next}
+    coordinates
+    # get A2 & :A1 into an array
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,30 @@
 require 'board'
 
 describe Board do
+  let(:cell){double :cell}
+  let(:second_cell){double :second_cell}
+  let(:cell_class){double :cell_class, new: cell}
+  let(:board) { Board.new({size: 100, cell: cell_class})}
+  let(:ship) { double :ship, size:1 }
+  let(:ship) { double :ship, size:1 }
+
   it 'can be created with default dimensions 1x1' do
-    expect(subject.dimensions).to eq 1
+    expect(board.grid.size).to eq 1
   end
 
   xit 'can place a ship'
+
+  it 'has 100 cells in the grid' do
+    expect(board.grid.count).to eq 100
+  end
+
+  it 'can place a size 2 ship on the cell' do
+    board.grid[:A1] = second_cell
+    board.grid[:A2] = second_cell
+    ship = double :ship, size: 2
+    expect
+  end
+
+  it "can work out the coordinates for a size" do
+  end
 end
